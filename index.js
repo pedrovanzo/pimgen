fc78d19d7ca41973ecefdb54d9c00be52c67aa3a
pedrovanzo <pedro_vanzo@hotmail.com>
2024-07-07 21:38:14 -0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,12 @@
+/**
+ * 
+ * @param {*} width px
+ * @param {*} height px
+ * @param {*} text string (optional)
+ * @param {*} color hexadecimal (optional)
+ * @returns 
+ */
+function pimgenGenerate(width, height, text, color) {
+    // img generation logic
+    return // image with width and height
+}
