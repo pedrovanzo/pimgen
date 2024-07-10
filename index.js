24008ddfb2cfb7d21e5dfff2f9899f1903b4c327
pedrovanzo <pedro_vanzo@hotmail.com>
2024-07-09 23:15:13 -0300

    feat: adds Image Generator
    BREAKING CHANGE: Execute the function with width and heigh parameters. Text and color parameters are
    optional.

--- index.js
+++ index.js
@@ -3,10 +3,45 @@
  * @param {*} width px
  * @param {*} height px
  * @param {*} text string (optional)
- * @param {*} color hexadecimal (optional)
+ * @param {*} color name, hex, hsl (optional)
  * @returns 
  */
 function pimgenGenerate(width, height, text, color) {
-    // img generation logic
-    return // image with width and height
+    // Creates canvas html element
+    const element = document.createElement('canvas');
+    const context = element.getContext('2d');
+    // Define canvas dimensions
+    element.width = width;
+    element.height = height;
+    // Fill in the background
+    if(!color) {
+        context.fillStyle = '#444444';
+    } else {
+        context.fillStyle = color;
+    }
+    context.fillRect(0, 0, element.width, element.height);
+    // Place the text
+    context.font = '400 1.75rem system-ui';
+    context.fillStyle = '#cccccc';
+    context.textAlign = 'center';
+    context.textBaseline = 'middle';
+    if(!text) {
+        context?.fillText(
+            `${element.width}x${element.height}`,
+            element.width / 2,
+            element.height / 2
+        );
+    } else {
+        context?.fillText(
+            `${text}`,
+            element.width / 2,
+            element.height / 2
+        );
+    }
+    // Set img from canvas
+    const imgFromCanvas = element.toDataURL('image/png', 1.0);
+    // return element;
+    return imgFromCanvas;
 }
+export default { pimgenGenerate }
+// module.exports = pimgenGenerate
